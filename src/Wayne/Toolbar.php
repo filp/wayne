7c67b3a5b034525da3669d436f2bfafbbdc35e89
filp <dobreira@gmail.com>
2013-03-17 23:24:25 +0000

    Add Wayne\Toolbar::render

--- src/Wayne/Toolbar.php
+++ src/Wayne/Toolbar.php
@@ -24,6 +24,16 @@ class Toolbar
     }
 
     /**
+     * Renders all registered widgets and the full toolbar,
+     * ready to ship off to a user-agent.
+     * @return string
+     */
+    protected function render()
+    {
+        return "not yet";
+    }
+
+    /**
      * @param Symfony\Component\HttpFoundation\Response $response
      */
     public function attachToResponseBody(Response $response)
@@ -34,8 +44,18 @@ class Toolbar
         // Apply heuristics (read: regular expressions) to attach
         // right before an existing </body> tag, if it exists. This
         // helps us prevent messing up non-html responses.
+        // 
+        // Regex searches are fast, so we do them twice: once to check
+        // there's actually something there, and one to do the replace.
+        // This is because the toolbar rendering may be a more or less
+        // intensive operation, and I'd rather avoid doing it for EVERY
+        // request.
         $bodyRe  = '/<\/body>/i';
-        $newContent = preg_replace($bodyRe, "wayne\n</body>", $content, 1);
-        $response->setContent($newContent);
+        if(preg_match($bodyRe, $content)) {
+            $toolbarContent = $this->render();
+            $newContent     = preg_replace($bodyRe, "{$toolbarContent}\n</body>", $content, 1);
+
+            $response->setContent($newContent);
+        }
     }
 }
