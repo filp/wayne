a6fbbfa7adf5f763f808c2ff5f807632f6f686ed
filp <dobreira@gmail.com>
2013-03-17 21:40:56 +0000

    Initial commit

---
+++ tests/Wayne/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * wayne - laravel4 debugger toolbar
+ * @author Filipe Dobreira <http://github.com/filp>
+ */
+namespace Wayne;
+
+class TestCase extends \PHPUnit_Framework_TestCase {}
---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * wayne - laravel4 debugger toolbar
+ * @author Filipe Dobreira <http://github.com/filp>
+ */
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
+$loader->add('Wayne\\', __DIR__);
