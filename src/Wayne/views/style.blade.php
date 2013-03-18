b84c4fa9d4997f4f197ffd8a4a9b70fbdd387e2d
filp <dobreira@gmail.com>
2013-03-18 01:02:01 +0000

    Add toolbar style template

---
+++ src/Wayne/views/style.blade.php
@@ -0,0 +1,6 @@
+<style>
+{{-- Stylesheet for the wayne toolbar
+     Includes the base style, and styles defined by individual
+     widgets, in order of arrival.
+--}}
+</style>
--- src/Wayne/views/toolbar.blade.php
+++ src/Wayne/views/toolbar.blade.php
@@ -1,6 +1,11 @@
-<div id="wayne-debug" class="wayne-toolbar">
+<div id="wayne-debug" class="wayne-toolbar-outer">
 <!-- You're seeing this toolbar because wayne is enabled.
      If you think this toolbar shouldn't have been visible for this request,
      please file an issue report @ https://github.com/filp/wayne -->
-  hello I'm a toolbar
+  @include('wayne::style')
+  <div class="wayne-toolbar">
+    <div class="widget-logo widget-static">
+      
+    </div>
+  </div>
 </div>
