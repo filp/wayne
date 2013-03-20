9ae44b793f6500a6851bcdec27ed6c6c2ec6f2bb
filp <dobreira@gmail.com>
2013-03-20 16:50:50 +0000

    Differentiate between buttons and badges

--- src/Wayne/views/widget/composite.blade.php
+++ src/Wayne/views/widget/composite.blade.php
@@ -6,9 +6,17 @@
   {{-- Render buttons/badgets defined for this widget --}}
   @section('buttons')
     @foreach($widget->part('buttons') as $button)
+      @if(!empty($button->href))
+      {{-- Does it have an href property? It's a clickable thing! --}}
       <a class="wayne-button" href="{{ $button->href }}" target="{{ $button->target }}" {{ $button->color ? "style=\"background: {$button->color}\"" : ''}}>
         {{ $button->label }}<!--
       --></a>
+      @else
+      {{-- Does it NOT have an href property? It's a stinkin' badge! --}}
+      <span class="wayne-button" {{ $button->color ? "style=\"background: {$button->color}\"" : ''}}>
+        {{ $button->label }}<!--
+      --></span>
+      @endif
     @endforeach
   @show
 
