dcac1d5ce1b580cca9565eb55c7cc370e2b209b3
filp <dobreira@gmail.com>
2013-03-20 17:10:16 +0000

    Remove sections

--- src/Wayne/views/widget/composite.blade.php
+++ src/Wayne/views/widget/composite.blade.php
@@ -1,31 +1,25 @@
 {{-- template used by the CompositeWidgetBuilder --}}
-@section('widget')
 <div class="wayne-widget" title="{{ $widget->part('title') }}" style="{{ $widget->part('style') }}">
   <div class="wayne-widget-body">  
 
   {{-- Render buttons/badgets defined for this widget --}}
-  @section('buttons')
-    @foreach($widget->part('buttons') as $button)
-      @if(!empty($button->href))
-      {{-- Does it have an href property? It's a clickable thing! --}}
-      <a class="wayne-button" href="{{ $button->href }}" target="{{ $button->target }}" {{ $button->color ? "style=\"background: {$button->color}\"" : ''}}>
-        {{ $button->label }}<!--
-      --></a>
-      @else
-      {{-- Does it NOT have an href property? It's a stinkin' badge! --}}
-      <span class="wayne-button" {{ $button->color ? "style=\"background: {$button->color}\"" : ''}}>
-        {{ $button->label }}<!--
-      --></span>
-      @endif
-    @endforeach
-  @show
-
-  @section('text')
-    @if($widget->hasPart('text'))
-    <span class="wayne-text">{{ $widget->part('text') }}</span>
+  @foreach($widget->part('buttons') as $button)
+    @if(!empty($button->href))
+    {{-- Does it have an href property? It's a clickable thing! --}}
+    <a class="wayne-button" href="{{ $button->href }}" target="{{ $button->target }}" {{ $button->color ? "style=\"background: {$button->color}\"" : ''}}>
+      {{ $button->label }}<!--
+    --></a>
+    @else
+    {{-- Does it NOT have an href property? It's a stinkin' badge! --}}
+    <span class="wayne-button" {{ $button->color ? "style=\"background: {$button->color}\"" : ''}}>
+      {{ $button->label }}<!--
+    --></span>
     @endif
-  @show
+  @endforeach
 
+  @if($widget->hasPart('text'))
+  <span class="wayne-text">{{ $widget->part('text') }}</span>
+  @endif
+  
   </div>
 </div>
-@show
